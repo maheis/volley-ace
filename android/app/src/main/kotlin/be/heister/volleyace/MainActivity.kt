ee2dad6a786104c131a6c048ab2c98e7afc4f2fd
maheis <manfred@heister.email>
2026-08-13 14:48:41 +0200

    Restore MainActivity.kt with initial FlutterActivity setup

---
+++ android/app/src/main/kotlin/be/heister/volleyace/MainActivity.kt
@@ -0,0 +1,5 @@
+package be.heister.volleyace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/volley_ace/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.volley_ace
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
